35d610ec5efa93aab29571f6c806e22614fb9a1b
HunterXProgrammer <113341550+HunterXProgrammer@users.noreply.github.com>
2023-09-14 14:16:05 +0530

    Update build-termux.sh

--- build-termux.sh
+++ build-termux.sh
@@ -12,7 +12,7 @@ dir_name="$(basename "$(pwd)")"
 sed 's/DISTRO_ARCH=$DEVICE_CPU_ARCH/DISTRO_ARCH=aarch64/' "$(which proot-distro 2>/dev/null)" | sed 's/\(distribution with alias.*already exists.*\)"/\1\n\n  Proceeding to use already installed distribution\."/g' > "$tmpfile"
 bash "$tmpfile" install --override-alias $distro-kernel-builder $distro | grep -Ev "Now run.*to log in"
 
-bash $tmpfile login --isolated --termux-home $distro-kernel-builder -- bash -c "apt update --allow-insecure-repositories 2>/dev/null && apt install --allow-unauthenticated -y curl python2 gcc gcc-arm-linux-gnueabi make zip bc libssl-dev xz-utils diffutils 2>&1 | grep -v -e warning -e WARNING -e Readline -e Dialog && cd \"\$HOME/$dir_name\" && bash build.sh termux"
+bash $tmpfile login --isolated --termux-home $distro-kernel-builder -- bash -c "apt update --allow-insecure-repositories 2>/dev/null && apt install --allow-unauthenticated -y git curl python2 gcc gcc-arm-linux-gnueabi make zip bc libssl-dev xz-utils diffutils 2>&1 | grep -v -e warning -e WARNING -e Readline -e Dialog && cd \"\$HOME/$dir_name\" && bash build.sh termux"
 
 rm $tmpfile
 
